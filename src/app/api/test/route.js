3d770cb9f735b72b8aa95fbeb3f62204e6aed29c
melpi-eg <melpi@eg.dk>
2024-12-03 22:19:14 +0530

    UI fixes

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,28 +1,11 @@
 "use server";
 import { NextResponse } from "next/server";
-import svg_to_ico from "svg-to-ico";
-import fs from "fs";
 
 export async function GET(req) {
   try {
-    const font = loadFont("fonts/GeistVF.woff");
-    const attributes = { fill: "red", stroke: "black" };
-    const options = {
-      font,
-      x: 0,
-      y: 0,
-      fontSize: 72,
-      anchor: "top",
-      attributes,
-    };
-
-    const svg = getSVG("hello", options);
-
-    console.log(svg);
     return NextResponse.json({ msg: "hello" });
   } catch (error) {
     console.log(error);
-    
     return NextResponse.json({ msg: error });
   }
 }
